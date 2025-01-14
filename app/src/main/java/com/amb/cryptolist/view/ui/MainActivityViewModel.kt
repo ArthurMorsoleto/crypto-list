28c4d1a1da65cabb1eca9a36f481eb14c81b1670
Arthur M Batista <arthur.morsoleto@gmail.com>
2025-01-14 15:14:55 -0300

    add main view state and get coins method to view model

--- app/src/main/java/com/amb/cryptolist/view/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/amb/cryptolist/view/ui/MainActivityViewModel.kt
@@ -1,13 +1,42 @@
 package com.amb.cryptolist.view.ui
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.amb.cryptolist.common.Response
 import com.amb.cryptolist.domain.usecase.GetCoinsUseCase
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel(
     private val getCoinsUseCase: GetCoinsUseCase
 ) : ViewModel() {
 
+    private var _state = mutableStateOf(ViewState())
+    val state: State<ViewState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        viewModelScope.launch {
+            _state.value = ViewState(isLoading = true)
+            when (val response = getCoinsUseCase()) {
+                is Response.Success -> {
+                    _state.value = ViewState(
+                        coins = response.data ?: emptyList(),
+                        isLoading = false
+                    )
+                }
+
+                is Response.Error -> {
+                    _state.value = ViewState(error = response.error, isLoading = false)
+                }
+            }
+        }
+    }
 
     companion object {
         val Factory = object : ViewModelProvider.Factory {
---
+++ app/src/main/java/com/amb/cryptolist/view/ui/ViewState.kt
@@ -0,0 +1,9 @@
+package com.amb.cryptolist.view.ui
+
+import com.amb.cryptolist.domain.model.CryptoCoin
+
+data class ViewState(
+    val isLoading: Boolean = true,
+    val coins: List<CryptoCoin> = emptyList(),
+    val error: Exception? = null
+)
